2b996d6897e47ddc256a04d343534a16df48580c
lpicanco <lpicanco@b4d21540-18f3-11de-9433-ad2e3fad96c5>
2009-03-25 04:20:15 +0000

    First Release

---
+++ trunk/TwitterNet/Services/TwitterFacade.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinqToTwitter;
+
+namespace TwitterNet.Services
+{
+    public class TwitterFacade
+    {
+        private TwitterContext context;
+
+        public bool Login(string login, string password)
+        {
+            bool result = true;
+
+            context = new TwitterContext(login, password);
+            
+            return result;
+        }
+
+        public IList<Status> ListUserTwitters()
+        {
+            var twitters = (from twitter in context.Status
+                           where twitter.Type == StatusType.User
+                           select twitter).ToList();
+
+            return twitters;
+        }
+
+        public IList<Status> ListTwitters()
+        {
+            var twitters = (from twitter in context.Status
+                            where twitter.Type == StatusType.Friends
+                            select twitter).ToList();
+
+            return twitters;
+        }
+
+
+        public IList<Status> ListReplies()
+        {
+            var twitters = (from twitter in context.Status
+                            where twitter.Type == StatusType.Replies
+                            select twitter).ToList();
+
+            return twitters;
+        }
+
+        public void UpdateStatus(string status)
+        {
+            context = new TwitterContext(context.UserName, context.Password);
+            context.UpdateStatus(status);
+        }
+    }
+}
